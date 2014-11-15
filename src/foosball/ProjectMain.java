96eb47c58c20584ad1b6d925e2cf55223497b228
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2014-11-15 19:38:53 +0530

    Added intellij files

--- src/foosball/ProjectMain.java
+++ src/foosball/ProjectMain.java
@@ -6,10 +6,8 @@ public class ProjectMain {
 	public static void main(String[] args){
 		StartingScreen myField = new StartingScreen();
 		
-		myField.setDefaultCloseOperation
-	         	(JFrame.EXIT_ON_CLOSE);
+		myField.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myField.pack();
 		myField.setVisible(true);
-		
 	}
 }
