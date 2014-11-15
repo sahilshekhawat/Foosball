3f2a0601931cbcf7f1136bae2a5ebe38e9a1647a
prikankshit <prikankshit13073@iiitd.ac.in>
2014-11-15 18:31:54 +0530

    Bouncing ball made but giving a null pointer exception don t know why. Need to be implemented on the field made by Shubham.

--- src/foosball/ProjectMain.java
+++ src/foosball/ProjectMain.java
@@ -5,7 +5,6 @@ import javax.swing.JFrame;
 public class ProjectMain {
 	public static void main(String[] args){
 		StartingScreen myField = new StartingScreen();
-		
 		myField.setDefaultCloseOperation
 	         	(JFrame.EXIT_ON_CLOSE);
 		myField.pack();
